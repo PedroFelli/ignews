99b64193519746029c83e5454139e23eb1542694
pedrofelli <pedrofellipeunai@gmail.com>
2022-03-27 15:05:19 -0300

    dinamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
-import styles from '../styles/home.module.scss';
+import Head from "next/head";
+
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return (<h1 className={styles.title}>Home news!</h1>);
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Home news!</h1>);
+    </>
+  );
 }
-import React from 'react';
+import React from "react";
