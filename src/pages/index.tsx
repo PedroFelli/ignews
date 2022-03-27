a907228da32a899f3157efcd54131fb7c94738df
pedrofelli <pedrofellipeunai@gmail.com>
2022-03-27 15:00:12 -0300

    add robot font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
-  return <h1>hello</h1>;
+  return (<h1 className={styles.title}>Home news!</h1>);
 }
+import React from 'react';
